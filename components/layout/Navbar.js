4b8e7b298f82eb2b0d0ad0f7ba1156665a621cc5
FraCanta <you@example.comfcantale14@gmail.com>
2024-03-19 17:45:54 +0100

    menu

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import Logo from "@/public/assets/logo.svg";
 import { Rubik } from "next/font/google";
+import { Icon } from "@iconify/react";
 const rubik = Rubik({ subsets: ["latin"] });
 
 const Navbar = () => {
@@ -11,19 +12,24 @@ const Navbar = () => {
       className={`w-full h-[80px] lg:h-[120px] py-4 lg:py-5 bg-lightGray shadow-lg justify-between items-center flex text-black font-normal ${rubik.className}`}
     >
       <nav className="w-[90%] mx-auto flex justify-between items-center">
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-1">
           <Image
             src={Logo}
             alt="logo tendeclissi"
             height={80}
             width={80}
-            className="h-14 w-14 lg:h-[80px] lg:w-[80px] "
+            className="h-14 w-14 lg:h-[70px] lg:w-[70px] fxl:h-[80px] fxl:w-[80px]  "
           />
           <p className="uppercase text-black font-medium hidden lg:block">
             tendeclissi
           </p>
         </div>
-        <ul className="lg:flex gap-[40px]  text-[23px] uppercase items-center leading-[37.50px] hidden ">
+        <Icon
+          icon="ion:menu-outline"
+          className="lg:hidden flex w-10 h-10 text-primary"
+        />
+
+        <ul className="lg:flex gap-[40px]  text-[20px] fxl:text-[23px] uppercase items-center leading-[37.50px] hidden ">
           <li>
             <Link href="/">Chi siamo</Link>
           </li>
