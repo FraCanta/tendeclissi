cc687622298ed7739684fa0c063c69f1c61f6b56
FraCanta <you@example.comfcantale14@gmail.com>
2024-03-21 16:21:50 +0100

    cards

--- components/layout/SimpleCard.js
+++ components/layout/SimpleCard.js
@@ -9,7 +9,7 @@ import {
 import CtaPrimary from "./CtaPrimary";
 const SimpleCard = () => {
   return (
-    <Card className="w-[90%] mx-auto lg:w-[65%] flex flex-col gap-5 shadow-lg">
+    <Card className="w-[90%] mx-auto lg:w-[65%] flex flex-col gap-5 shadow-lg p-6">
       <CardBody>
         <Typography variant="h5" color="blue-gray" className="mb-2">
           Cappottine
