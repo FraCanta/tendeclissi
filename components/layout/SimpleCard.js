db097b519ec22207ecff0264db41ee514a0c87b1
FraCanta <you@example.comfcantale14@gmail.com>
2024-03-22 18:30:02 +0100

    cards

--- components/layout/SimpleCard.js
+++ components/layout/SimpleCard.js
@@ -14,7 +14,7 @@ const SimpleCard = ({ title, description, cta }) => {
         <Typography
           variant="h4"
           color="blue-gray"
-          className="mb-1 font-bold text-[28px] md:text-[35px] lg:text-[22px] 2xla:text-[25px] fxl:text-[32px] fxl:mb-2"
+          className="mb-1 font-bold text-[25px] md:text-[35px] lg:text-[22px] 2xla:text-[25px] fxl:text-[32px] fxl:mb-2"
         >
           {title}
         </Typography>
