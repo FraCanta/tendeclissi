70973809fe84d99727ccac6369821e80e1c7bb14
FraCanta <you@example.comfcantale14@gmail.com>
2024-03-29 18:49:01 +0100

    visualizzazioni

--- components/layout/MediaCards.js
+++ components/layout/MediaCards.js
@@ -28,14 +28,14 @@ const MediaCards = ({
           <Image src={img} fill alt="img di prova" className="object-cover" />
           <div className="absolute inset-0 bg-black opacity-50"></div>
 
-          <div className="absolute inset-0 flex flex-col items-center xl:justify-center gap-4 lg:text-center text-white mt-10 sm:my-24 md:mt-[90px] lg:mt-[150px] xl:mt-0 h-full">
+          <div className="absolute inset-0 flex flex-col items-center xl:justify-center gap-4 lg:text-center text-white mt-24 sm:mt-32 md:mt-[90px] lg:mt-[150px] xl:mt-0 h-full">
             <h2 className="text-white text-[30px] md:text-[44px] lg:text-[45px] xl:text-[35px] 2xl:text-[45px] z-10 w-[90%] xl:w-[70%] 2xl:w-[65%] 2xla:w-[60%] mx-auto font-bold">
               {mainTitle}
             </h2>
             <p className="z-10 w-[90%] xl:w-[60%] fxl:w-[55%] mx-auto sm:text-xl md:text-[22px] xl:text-xl">
               {mainDescription}
             </p>
-            <div className="xl:hidden absolute bottom-20 sm:bottom-40  md:bottom-40 md:left-10 lg:bottom-52 xl:left-16 w-[90%] left-5 right-0  text-darkGray ">
+            <div className="xl:hidden absolute bottom-32 sm:bottom-40  md:bottom-40 md:left-10 lg:bottom-52 xl:left-16 w-[90%] left-5 right-0  text-darkGray ">
               <Swiper
                 slidesPerView={1}
                 spaceBetween={20}
