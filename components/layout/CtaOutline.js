ab7d1fb93eefac969bd90167c8b9af4f692e398e
FraCanta <you@example.comfcantale14@gmail.com>
2024-04-11 19:44:59 +0200

    buttons

--- components/layout/CtaOutline.js
+++ components/layout/CtaOutline.js
@@ -4,7 +4,7 @@ import React from "react";
 const CtaOutline = ({ link, children }) => {
   return (
     <Link
-      className="border border-white py-[15px] px-[33px] text-white uppercase md:text-[25px] xl:text-lg 2xl:text-xl fxl:text-[25px] max-w-max"
+      className="border border-white  py-[15px] px-[20px] lg:px-[33px] text-white uppercase text-base md:text-[25px] lg:text-xl xl:text-base 2xl:text-xl fxl:text-[25px] max-w-max"
       href={link}
     >
       {children}
--- components/layout/CtaSecondary.js
+++ components/layout/CtaSecondary.js
@@ -5,7 +5,7 @@ const CtaSecondary = ({ children, link }) => {
   return (
     <Link
       href={link}
-      className="bg-white py-[15px] px-[33px] text-primary max-w-max uppercase font-medium z-10 md:text-[25px] lg:text-xl fxl:text-[25px]"
+      className="bg-white  py-[15px] px-[20px] lg:px-[33px] text-primary max-w-max uppercase font-medium z-10 text-base md:text-[25px] lg:text-xl xl:text-base 2xl:text-xl fxl:text-[25px]"
     >
       {children}
     </Link>
