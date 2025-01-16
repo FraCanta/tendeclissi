895ad07e87a885ab266cce8d3a032eb3165fa709
FraCanta <fcantale14@gmail.com>
2025-01-16 15:43:54 +0100

    finito ma con revisione

--- components/forms/FormEco.js
+++ components/forms/FormEco.js
@@ -10,15 +10,15 @@ const FormEco = () => {
           Ottieni l&apos;ecobonus sulle tende da sole con noi!
         </h2>
         <p
-          className="text-white text-xl max-w-6xl mx-auto"
+          className="max-w-6xl mx-auto text-xl text-white"
           dangerouslySetInnerHTML={{
             __html:
               "Per ottenere la detrazione fiscale sulle tende da sole, <strong>inviaci un’email</strong> indicando gli interventi che vorresti dichiarare: ti aiuteremo a individuare tutte le <strong>spese includibili</strong> nel bonus e ti forniremo tutti i <strong>dettagli tecnici dei prodotti</strong>.",
           }}
         ></p>
         <form className="w-full max-w-3xl mx-auto lg:my-12">
-          <div className="flex flex-col lg:flex-row -mx-3 mb-6 gap-4">
-            <div className="w-full md:w-1/2 px-3 p-2 bg-white border border-yellow">
+          <div className="flex flex-col gap-4 mb-6 -mx-3 lg:flex-row">
+            <div className="w-full p-2 px-3 bg-white border md:w-1/2 border-yellow">
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-purple  px-[33px] py-[15px]  leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
@@ -26,7 +26,7 @@ const FormEco = () => {
                 placeholder="Nome"
               />
             </div>
-            <div className="w-full md:w-1/2 px-3 p-2 bg-white border border-yellow">
+            <div className="w-full p-2 px-3 bg-white border md:w-1/2 border-yellow">
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-purple  px-[33px] py-[15px]  leading-tight focus:outline-none focus:bg-white"
                 id="grid-last-name"
@@ -35,8 +35,8 @@ const FormEco = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row -mx-3 gap-4 ">
-            <div className="w-full md:w-1/2 px-3 p-2 bg-white border border-yellow">
+          <div className="flex flex-col gap-4 -mx-3 lg:flex-row ">
+            <div className="w-full p-2 px-3 bg-white border md:w-1/2 border-yellow">
               <input
                 className="appearance-none block w-full bg-white text-gray-700 border border-purple  px-[33px] py-[15px]  leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
@@ -44,7 +44,7 @@ const FormEco = () => {
                 placeholder="Numero di telefono"
               />
             </div>
-            <div className="w-full md:w-1/2 px-3 p-2 flex items-center">
+            <div className="flex items-center w-full p-2 px-3 md:w-1/2">
               <button
                 className="flex w-full items-center justify-center uppercase 3xl:text-[35px] 4xl:text-[55px]  px-[33px] py-[15px]  md:text-[25px] lg:text-xl fxl:text-[25px]   bg-primary text-white font-regular"
                 type="submit"
@@ -56,7 +56,7 @@ const FormEco = () => {
         </form>
       </div>
       <div className="w-[90%] mx-auto py-8 lg:py-0">
-        <div className=" max-w-3xl mx-auto">
+        <div className="max-w-3xl mx-auto ">
           <label className="text-white ">
             <input type="checkbox" />{" "}
             <span
@@ -66,13 +66,6 @@ const FormEco = () => {
               }}
             ></span>
           </label>
-        </div>
-      </div>
-      <div className="w-[90%] mx-auto flex justify-center items-center py-6">
-        <div className="w-full lg:max-w-3xl mx-auto justify-evenly lg:justify-center flex items-center lg:gap-6 text-white">
-          <Icon icon="uil:facebook" className="w-6 h-6" />
-          <Icon icon="raphael:instagram" className="w-6 h-6" />
-          <Icon icon="mingcute:whatsapp-fill" className="w-6 h-6" />
         </div>
       </div>
     </>
