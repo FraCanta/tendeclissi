c8965e1d4b4ef1b0a087ccaeb7a214e2742e795d
FraCanta <fcantale14@gmail.com>
2025-09-05 15:03:50 +0200

    spostato iubenda sopra google tag manager

--- next.config.js
+++ next.config.js
@@ -23,12 +23,13 @@ const nextConfig = {
 };
 
 const ContentSecurityPolicy = `
-  default-src 'self' https://idb.iubenda.com/ ;
-  script-src 'self' https://embeds.iubenda.com/ http://cdn.iubenda.com/ https://cs.iubenda.com/ https://www.googletagmanager.com/ 'unsafe-inline' 'unsafe-eval';
-  child-src 'self' https://www.iubenda.com/ ;
-  style-src 'self' https://fonts.googleapis.com/  'unsafe-inline'   data:;
-  font-src 'self' https://fonts.gstatic.com/ 'unsafe-inline' data:;
-  img-src 'self'  data: blob:;
+  default-src 'self' https://idb.iubenda.com;
+  script-src 'self' https://embeds.iubenda.com https://cdn.iubenda.com https://cs.iubenda.com https://www.googletagmanager.com 'unsafe-inline' 'unsafe-eval';
+  child-src 'self' https://www.iubenda.com;
+  style-src 'self' https://fonts.googleapis.com 'unsafe-inline' data:;
+  font-src 'self' https://fonts.gstatic.com data:;
+  img-src 'self' data: blob:;
+  connect-src 'self' https://idb.iubenda.com https://cpl.iubenda.com https://api.iconify.design https://api.simplesvg.com https://api.unisvg.com https://region1.google-analytics.com;
 `;
 
 const securityHeaders = [
